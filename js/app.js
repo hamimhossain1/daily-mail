5cc287537e0350fb69b325d665bec15547ade9f2
HamimHossain <hamim.mgp@gmail.com>
2022-09-03 00:08:36 +0600

    js code has trart

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('clicked')
+
+const loadAllCatagories = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
+    const data = await res.json();
+    return data;
+}
+
+const displayAllCatagories = async () => {
+    const data = await loadAllCatagories();
+    const newsCatagories = document.getElementById('news-category');
+    data.forEach(category => {
+
+        newsCatagories.innerHTML = `
+        
+        `
+    });
+
+    console.log(data)
+}
+
+displayAllCatagories();
