ec84417fe3ddb9f5a1502a6ddc5bd71da2596bcd
HamimHossain <hamim.mgp@gmail.com>
2022-09-03 08:46:14 +0600

    news catagories display done

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
-
+// ---load all catagories ---//
 const loadAllCatagories = async () =>{
     const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
     const data = await res.json();
     return data;
 }
 
-const displayAllCatagories = async () => {
+// ---display all catagories ---//
+const displayAllCatagories = async (category) => {
     const data = await loadAllCatagories();
     // console.log(data.data.news_category);
     const newsCatagories = document.getElementById('news-category');
-    data.data.news_category.forEach(category => {
-        const  {category_name} = category
-        console.log(category_name)
-
-        newsCatagories.innerHTML = `
-        <h1>Hello world</h1>
+    data.data.news_category.forEach((category) => {
+        const  {category_name} = category;
+        // console.log(category.category_name)
+        const div = document.createElement('div');
+        div.classList.add('inline-block')
+        div.innerHTML = `
+        <button type="button" class="btn  ">${category_name}</button>
         `
+        newsCatagories.appendChild(div)
     });
-
-    // console.log(data)
 }
 
 displayAllCatagories();
